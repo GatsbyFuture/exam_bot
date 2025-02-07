71df2fc8329a34ea3813cb49dd0761b8826f151d
Jack <raxmatoveldor37@gmail.com>
2025-02-07 23:10:12 +0500

    The structure was created and seperated special modules

---
+++ root/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    group_id: -235113124,
+
+    db_port: '127.0.0.1',
+    db_name: 'exam',
+    db_user: 'root',
+    db_pass: 'root-super-pwd',
+
+    bot_token: 'qwerty',
+    bot_username: 'qwerty',
+
+    roles: {
+        ADMIN: 'admin',
+        USER: 'user',
+    },
+}
---
+++ root/core/middleware/middleware.primary.js
@@ -0,0 +1,7 @@
+const updateTime = async () =>{
+    // all events will pass here
+}
+
+module.exports = {
+    updateTime,
+}
